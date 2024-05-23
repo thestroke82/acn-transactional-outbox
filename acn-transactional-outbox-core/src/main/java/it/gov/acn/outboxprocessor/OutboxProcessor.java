11c1a66739991bfa4e43ec3ee3eddca19bcbe13f
thestroke82 <fabio.trabattoni@gmail.com>
2024-05-23 08:18:36 +0200

    refactor: outbox processor and configuration

--- acn-transactional-outbox-core/src/main/java/it/gov/acn/outboxprocessor/OutboxProcessor.java
+++ acn-transactional-outbox-core/src/main/java/it/gov/acn/outboxprocessor/OutboxProcessor.java
@@ -5,17 +5,17 @@ import org.slf4j.LoggerFactory;
 
 public class OutboxProcessor {
     private Logger logger = LoggerFactory.getLogger(OutboxProcessor.class);
-    private ProcessOutboxCommand command;
+    private OutboxProcessorConfiguration command;
 
-    public OutboxProcessor(ProcessOutboxCommand command) {
+    public OutboxProcessor(OutboxProcessorConfiguration command) {
         this.command = command;
-        if(this.command == null || this.command.getMessage() == null){
+        if(this.command == null || this.command.getTestString() == null){
             throw new IllegalArgumentException("Command or message cannot be null");
         }
     }
 
     public void process() {
-        logger.info("Processing outbox, they told me to say {}",this.command.getMessage());
+        logger.info("Processing outbox, they told me to say {}",this.command.getTestString());
     }
 
 
