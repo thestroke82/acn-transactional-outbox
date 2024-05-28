f962e3ec9341d38193f78947d5e96b92c9590ffc
thestroke82 <fabio.trabattoni@gmail.com>
2024-05-28 14:14:39 +0200

    fix: updated default configuration

--- acn-transactional-outbox-autoconfigure/src/main/java/it/gov/acn/autoconfigure/outbox/config/DefaultConfiguration.java
+++ acn-transactional-outbox-autoconfigure/src/main/java/it/gov/acn/autoconfigure/outbox/config/DefaultConfiguration.java
@@ -2,10 +2,10 @@ package it.gov.acn.autoconfigure.outbox.config;
 
 public class DefaultConfiguration {
     public static final boolean ENABLED = false;
-    public static final long FIXED_DELAY = 5000; // milliseconds
+    public static final long FIXED_DELAY = 30000; // milliseconds
     public static final String TABLE_NAME = "transactional_outbox";
 
     public static final int MAX_ATTEMPTS = 3;
 
-    public static final int BACKOFF_BASE = 5*60; // 5 minutes, it's in seconds
+    public static final int BACKOFF_BASE = 5; // minutes
 }
