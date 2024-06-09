ba662cd292f8aea7ebb72c266e0795cb9a88ba8e
Fabio Trabattoni <fabio.trabattoni@gmail.com>
2024-06-09 16:27:45 +0200

    fix: TransactionTemplateProvider logging

--- acn-transactional-outbox-autoconfigure/src/main/java/it/gov/acn/autoconfigure/outbox/providers/transaction/TransactionTemplateProvider.java
+++ acn-transactional-outbox-autoconfigure/src/main/java/it/gov/acn/autoconfigure/outbox/providers/transaction/TransactionTemplateProvider.java
@@ -2,17 +2,13 @@ package it.gov.acn.autoconfigure.outbox.providers.transaction;
 
 import it.gov.acn.outbox.provider.TransactionManagerProvider;
 import org.slf4j.Logger;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
+import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
-import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.util.Assert;
 
 public class TransactionTemplateProvider implements TransactionManagerProvider {
-    private Logger logger = org.slf4j.LoggerFactory.getLogger(TransactionTemplateProvider.class);
+    private Logger logger = LoggerFactory.getLogger(TransactionTemplateProvider.class);
     @Override
     @Transactional
     public void executeInTransaction(Runnable runnable) {
@@ -22,10 +18,10 @@ public class TransactionTemplateProvider implements TransactionManagerProvider {
     }
 
      private void logTransactionStatus(String phase) {
-        logger.info("[{}] Transaction active: {}", phase, TransactionSynchronizationManager.isActualTransactionActive());
-        logger.info("[{}] Synchronization active: {}", phase, TransactionSynchronizationManager.isSynchronizationActive());
-        logger.info("[{}] Current transaction name: {}", phase, TransactionSynchronizationManager.getCurrentTransactionName());
-        logger.info("[{}] Current transaction isolation level: {}", phase, TransactionSynchronizationManager.getCurrentTransactionIsolationLevel());
-        logger.info("[{}] Current transaction read-only: {}", phase, TransactionSynchronizationManager.isCurrentTransactionReadOnly());
+        logger.trace("[{}] Transaction active: {}", phase, TransactionSynchronizationManager.isActualTransactionActive());
+        logger.trace("[{}] Synchronization active: {}", phase, TransactionSynchronizationManager.isSynchronizationActive());
+        logger.trace("[{}] Current transaction name: {}", phase, TransactionSynchronizationManager.getCurrentTransactionName());
+        logger.trace("[{}] Current transaction isolation level: {}", phase, TransactionSynchronizationManager.getCurrentTransactionIsolationLevel());
+        logger.trace("[{}] Current transaction read-only: {}", phase, TransactionSynchronizationManager.isCurrentTransactionReadOnly());
     }
 }
