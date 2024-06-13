d9442984cd0de76f6e9597f2a12a71dbe133c9dc
thestroke82 <fabio.trabattoni@gmail.com>
2024-06-13 09:08:52 +0200

    fix: autoconfiguration to be run after prometheus

--- acn-transactional-outbox-example-app/src/main/java/it/gov/acn/conf/ApplicationConfiguration.java
+++ acn-transactional-outbox-example-app/src/main/java/it/gov/acn/conf/ApplicationConfiguration.java
@@ -2,16 +2,27 @@ package it.gov.acn.conf;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfiguration {
 
+    @Value("${spring.application.name:example-app}")
+    private String applicationName;
+
     @Bean
     ObjectMapper jacksonObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
+    }
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", this.applicationName);
     }
 }
